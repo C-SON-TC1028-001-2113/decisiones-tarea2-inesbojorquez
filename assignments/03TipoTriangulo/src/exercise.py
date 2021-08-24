bd5e12fefb4552743e43bbbfade301b55e594d5a
inesbojorquez <a01252378@itesm.mx>
2021-08-24 01:14:38 +0000

    02Licencia

--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -1,6 +1,23 @@
 def main():
     #escribe tu código abajo de esta línea
-    
+    x = int(input('Lado x:'))
+    y = int(input('Lado y:'))
+    z = int(input('Lado z:'))
+      
+    if x>0 and y>0 and z>0:
+        if x+y>z and x+z>y and y+z>x:
+             if x==y and x==z:
+                  print('ES UN TRIANGULO EQUILATERO')
+             elif x==y or x==z or y==z:
+                 print('ES UN TRIANGULO ISOSCELES') 
+             else:
+                 print('ES UN TRIANGULO ESCALENO')
+        else:
+            print('NO ES UN TRIANGULO')
+    else:
+        print('NO ES UN TRIANGULO')
+    input('')
+
 
 
 if __name__=='__main__':
